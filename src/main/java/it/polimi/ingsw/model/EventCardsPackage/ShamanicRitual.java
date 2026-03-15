46ec2d4c7065fae6e6c7513ee0d2b01b55cf543b
pizzri <ricky04p@gmail.com>
2026-03-15 20:30:19 +0100

    Completed implementation of ShamanicRitual.

--- src/main/java/it/polimi/ingsw/model/EventCardsPackage/ShamanicRitual.java
+++ src/main/java/it/polimi/ingsw/model/EventCardsPackage/ShamanicRitual.java
@@ -4,6 +4,8 @@ import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Game;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShamanicRitual extends EventCard {
     private final int victoryPrestigePoints;
@@ -16,9 +18,25 @@ public class ShamanicRitual extends EventCard {
 
     @Override
     protected void eventHandler(Player player){
+        int maxStars = Integer.MIN_VALUE;
+        int minStars = Integer.MAX_VALUE;
         Map<Player, Integer> shamanicStarsForPlayer = new HashMap<>();
-        for(Player pl: Game.game().getPlayers())
-            shamanicStarsForPlayer.put(pl, pl.getTribe().getShamanicStars());
+        List<Player> maxPlayers = new ArrayList<>();
+        List<Player> minPlayers = new ArrayList<>();
 
+        for(Player pl: Game.game().getPlayers()) shamanicStarsForPlayer.put(pl, pl.getTribe().getShamanicStars());
+
+        for (int stars : shamanicStarsForPlayer.values()) {
+            if (stars > maxStars) maxStars = stars;
+            if (stars < minStars) minStars = stars;
+        }
+
+        for (Map.Entry<Player, Integer> entry : shamanicStarsForPlayer.entrySet()) {
+            if (entry.getValue() == maxStars) maxPlayers.add(entry.getKey());
+            if (entry.getValue() == minStars) minPlayers.add(entry.getKey());
+        }
+
+        for (Player pl : maxPlayers) pl.changePrestigePoints(victoryPrestigePoints);
+        for (Player pl : minPlayers) pl.changePrestigePoints(defeatPrestigePoints);
     }
 }
