69f8594967a07fda759bea15b6a30f4552204a92
ClaCla2702 <99445818+ClaCla2702@users.noreply.github.com>
2026-03-16 17:12:14 +0100

    Board

--- src/main/java/it/polimi/ingsw/model/Interfaces/TribeDeck.java
+++ src/main/java/it/polimi/ingsw/model/Interfaces/TribeDeck.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.model.Interfaces;
 
+/**
+ * Marker interface for cards that can be placed in the tribe deck
+ * and in the board's upper and lower tribe rows.
+ *
+ * Implemented by:
+ *  - CharacterCard
+ *  - EventCard
+ */
 public interface TribeDeck {
 }
